8ccf74e1ff3692a80de6bf449a34f9c4958f650d
Christine Lee <christinelee622@gmail.com>
2025-11-16 18:04:33 -0500

    W13 Project

--- Program.cs
+++ Program.cs
@@ -3,6 +3,7 @@ using Microsoft.Extensions.DependencyInjection;
 using MvcBookshelf.Data;
 using MvcBookshelf.Models;
 using MvcBookshelf.Services;
+using Microsoft.Extensions.Diagnostics.HealthChecks;
 
 namespace MvcBookshelf
 {
@@ -14,14 +15,28 @@ namespace MvcBookshelf
             builder.Services.AddDbContext<MvcBookshelfContext>(options =>
                 options.UseSqlServer(builder.Configuration.GetConnectionString("MvcBookshelfContext") ?? throw new InvalidOperationException("Connection string 'MvcBookshelfContext' not found.")));
             // add scoped with bookshelf service
+
+            // add health check services before the builder https://www.youtube.com/watch?v=9ntrl3KNCpo
+            // utilize EntityFramework core DbContext probe https://learn.microsoft.com/en-us/aspnet/core/host-and-deploy/health-checks?view=aspnetcore-9.0#entity-framework-core-dbcontext-probe
+            // and install the correct NuGet package, with .NET version of 9.0 instead of latest 10.0 for compatability
+            builder.Services.AddHealthChecks().AddDbContextCheck<MvcBookshelfContext>(); // otherwise add health check for SQL serv
+            // custom health check using SampleHealthCheck isn't needed with AddDbContextCheck
+            // builder.Services.AddHealthChecks().AddCheck<SampleHealthCheck>("Sample");
             builder.Services.AddScoped<IBookshelfService,BookshelfService>();
 
             // Add services to the container.
             builder.Services.AddControllersWithViews();
 
+            // read secret via Configuration API
+            // https://learn.microsoft.com/en-us/aspnet/core/security/app-secrets?view=aspnetcore-9.0&tabs=windows
+            var mvcBookshelfSecret = builder.Configuration["MvcBookshelf:ServiceApiKey"];
+
 
 
             var app = builder.Build();
+
+            // map into app api endpoint
+            app.MapHealthChecks("/healthz");
 
             using (var scope = app.Services.CreateScope())
             {
@@ -45,10 +60,13 @@ namespace MvcBookshelf
             app.UseAuthorization();
 
             app.MapStaticAssets();
+            // app.MapGet("/", () => mvcBookshelfSecret); don't need to read secret
             app.MapControllerRoute(
                 name: "default",
                 pattern: "{controller=Home}/{action=Index}/{id?}")
                 .WithStaticAssets();
+
+
 
             app.Run();
         }
---
+++ SampleHealthCheck.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.Extensions.Diagnostics.HealthChecks;
+
+// redundant - custom health check not needed using AddDbContext for health check
+namespace MvcBookshelf
+{
+    public class SampleHealthCheck : IHealthCheck
+    {
+        public Task<HealthCheckResult> CheckHealthAsync(
+            HealthCheckContext context, CancellationToken cancellationToken = default)
+        {
+            // suggested code to check health with boolean
+            var isHealthy = true;
+
+            if (isHealthy)
+            {
+                return Task.FromResult(HealthCheckResult.Healthy("A healthy result.")); // is healthy
+            }
+            return Task.FromResult(HealthCheckResult.Unhealthy("An unhealthy result.")); // otherwise isn't
+        }
+    }
+
+}
